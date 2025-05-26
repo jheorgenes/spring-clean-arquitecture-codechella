11bb660c7ea99e1ac2bbac0e51a16b1e8f2e3660
Jheorgenes Warlley <jheorgenes@gmail.com>
2025-05-26 17:42:44 -0300

    - Definindo uma arquitetura correta seguindo o Clean Arquitecture para cadastrar um usuário, usando Application, Domain e Infra e separando cada camada corretamente, criando o isolamento entre a lógica, a aplicação e a infraestrutura (frameworks, etc).

---
+++ src/main/java/br/com/alura/codechella/infra/persistence/UsuarioRepository.java
@@ -0,0 +1,5 @@
+package br.com.alura.codechella.infra.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> { }
